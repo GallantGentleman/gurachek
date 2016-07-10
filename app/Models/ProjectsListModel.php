0bd46202023897d6cedceb378086294b21b38f96
GallantGentleman <webcrash@mail.ua>
2016-07-10 14:20:14 +0300

    add

--- app/Models/ProjectsListModel.php
+++ app/Models/ProjectsListModel.php
@@ -5,7 +5,7 @@ namespace app\models;
 class ProjectsListModel extends \riki\core\Model {
 
   public function getData() {
-
+      $stmt = $this->model->prepare("SELECT * FROM  ");
   }
 
 }
--- vendor/riki/core/Model.php
+++ vendor/riki/core/Model.php
@@ -4,6 +4,12 @@ namespace riki\core;
 
 abstract class Model {
 
+    private $model;
+
+    function __construct() {
+        $this->model = new PDO("mysql:host=localhost;dbname=pp-manager", "root", "");
+    }
+
     abstract function getData();
 
 }
